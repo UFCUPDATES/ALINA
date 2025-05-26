cc079019fadda63e49d74da4caea874f018d7a5b
UFC_UPDATES <ufcupdates313@gmail.com>
2025-05-26 11:38:22 +0530

    Update play.py

--- RishuMusic/utils/inline/play.py
+++ RishuMusic/utils/inline/play.py
@@ -52,7 +52,7 @@ def stream_markup_timer(_, chat_id, played, dur):
     elif 80 <= umm < 95:
         bar = "▰▰▰▰▰▰▰▰▰▰"
     else:     
-	bar = ""
+	bar = "▰▰▰▰▰▰▰▰▰▰"
     else:
     buttons = [
                 [
